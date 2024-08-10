df74569bffa1f7c8982426dbe83ca55967872540
Zcc <x3513312668@gmail.com>
2024-08-10 21:23:50 +0800

    pre-release 1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
 plugins {
     id("java")
     kotlin("jvm")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    `maven-publish`
 }
 
-group = "cc.mcyx.arona"
-version = "1.0-SNAPSHOT"
+group = "cc.mcyx"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
+    mavenLocal()
     maven("https://maven.aliyun.com/repository/public")
     maven("https://repo.papermc.io/repository/maven-public/")
 }
@@ -15,8 +18,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation(kotlin("stdlib-jdk8"))
-    implementation("cn.hutool:hutool-all:5.8.29")
+    compileOnly(kotlin("stdlib-jdk8"))
+    compileOnly("cn.hutool:hutool-all:5.8.29")
     compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
 }
 
@@ -30,4 +33,21 @@ kotlin {
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+        }
+    }
+    repositories {
+        maven {
+            url = uri(layout.buildDirectory.dir("repo"))
+        }
+    }
 }
