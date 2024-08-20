c565952c61feec49207c292d149acd1b28f1751b
Zcc <x3513312668@gmail.com>
2024-08-20 21:50:54 +0800

    给玩家发送数据包

--- Arona-NMS/src/main/kotlin/cc/mcyx/arona/nms/PlayerNMS.kt
+++ Arona-NMS/src/main/kotlin/cc/mcyx/arona/nms/PlayerNMS.kt
@@ -1,5 +1,6 @@
 package cc.mcyx.arona.nms
 
+import cc.mcyx.arona.nms.craftbukkit.CraftBukkitPacket
 import org.bukkit.entity.Player
 
 
@@ -8,5 +9,13 @@ import org.bukkit.entity.Player
  * @param packet 数据包类
  */
 fun Player.sendPacket(packet: Any) {
-
+    CraftBukkitPacket.craftPlayer.cast(this).apply {
+        val handle = javaClass.getDeclaredMethod("getHandle").invoke(this)
+        val playerConnection = CraftBukkitPacket.getObject(handle, "PlayerConnection")
+        playerConnection.javaClass.getDeclaredMethod(
+            if (CraftBukkitPacket.serverId > 1170) "a" else "sendPacket",
+            CraftBukkitPacket.packet
+        )
+            .also { it.isAccessible = true }.invoke(playerConnection, packet)
+    }
 }
